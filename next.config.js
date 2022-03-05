023e228300e5439d14a9b36af53c1df87ad7ae28
Anthony <90chanho@gmail.com>
2022-03-06 00:01:43 +0900

    deploy setting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa");
+
 const nextConfig = {
   poweredByHeader: process.env.NODE_ENV === "development",
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+  },
   reactStrictMode: process.env.NODE_ENV === "development",
 };
 
