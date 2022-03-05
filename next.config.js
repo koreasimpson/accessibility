8f1dc9052f564612a61f2e270b6f9adf451c1b30
Anthony <90chanho@gmail.com>
2022-03-05 23:50:29 +0900

    next config set

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  poweredByHeader: process.env.NODE_ENV === "development",
+  reactStrictMode: process.env.NODE_ENV === "development",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
